b4694602a98312d01a749534dae5c2ace27e1c8f
Alexander Antoshkin <ighost99@gmail.com>
2019-04-24 16:30:00 +0300

    Temporary commit.

--- project/src/WaveEquation.cpp
+++ project/src/WaveEquation.cpp
@@ -25,21 +25,9 @@ void crossScheme(double _t, double _h, string _path, int _testNum) {
     V0[0] = phiFunction(0, _testNum);
     V0[V0.size() - 1] = psiFunction(0, _testNum);
     fOut << V0[0] << " ";
-//    cout << "Size of V0 = " << V0.size() << endl;
     for (int i = 1; i < V0.size() - 1; ++i) {
         double x = i * _h;
-//        cout << "x0 = " << x << endl;
         V0[i] = zeroFunction(x, _testNum);
-        cout << i * _h << endl;
-//        if (fabs(i * _h - 1) < 1e-5) {
-//            cout << "qq" << endl;
-//            V0[i] = 0.0;
-//        }
-//        cout << "V0[i] = " << V0[i] << endl;
-//        if (fabs(V0[i] - V0[i - 1]) > 1e-1) {
-//            V0[i] = fabs(V0[i] - V0[i - 1]) / 2.0;
-//        }
-//        cout << "V0[i] = " << V0[i] << endl;
         fOut << V0[i] << " ";
     }
     fOut << V0[V0.size() - 1] << endl;
@@ -54,12 +42,6 @@ void crossScheme(double _t, double _h, string _path, int _testNum) {
     for (int i = 1; i < V1.size() - 1; ++i) {
         double x = i * _h;
         V1[i] = V0[i] + _t * gFunction(x, _testNum) + (a * a * _t * _t / 2.0) * zeroDerivative(x, _testNum);
-//        if (fabs(i * _h - 1) < 1e-5) {
-//            V1[i] = 0.0;
-//        }
-//        if (fabs(V1[i] - V1[i - 1]) > 1e-1) {
-//            V1[i] = fabs(V1[i] - V1[i - 1]) / 2.0;
-//        }
         fOut << V1[i] << " ";
     }
     fOut << V1[V1.size() - 1] << endl;
@@ -76,13 +58,6 @@ void crossScheme(double _t, double _h, string _path, int _testNum) {
         // Middle part
         for (int n = 1; n < nextLayer.size() - 1; ++n) {
             nextLayer[n] = 2 * V1[n] - V0[n] + courantNumberSqr * (V1[n + 1] - 2 * V1[n] + V1[n - 1]);
-            //cout << "n = " << n << endl;
-//            if (fabs(n * _h - 1) < 1e-5) {
-//                nextLayer[n] = 0.0;
-//            }
-//            if (fabs(nextLayer[n] - nextLayer[n - 1]) > 1e-1) {
-//                nextLayer[n] = fabs(nextLayer[n] - nextLayer[n - 1]) / 2.0;
-//            }
             fOut << nextLayer[n] << " ";
         }
         fOut << nextLayer[nextLayer.size() - 1] << endl;
@@ -172,7 +147,6 @@ double exactSolution(double _t, double _x, int _testNum) {
         case 2: {
             double eps = 1e-7;
             double k = 0.5 * (sqrt(2.0 / (Pi * Pi * eps)) - 1);
-            //cout << "k = " << k << endl;
             double sum = 0.0;
             for (int n = 0; n <= k; ++n) {
                 sum += 1 / pow(2 * n + 1, 3) * sin((2 * n + 1) * Pi * _x) * cos((2 * n + 1) * Pi * _t);
@@ -180,7 +154,6 @@ double exactSolution(double _t, double _x, int _testNum) {
             return 8.0 / pow(Pi, 3) * sum;
         }
         case 3: {
-            //cout << "Third test hasn't exact solution" << endl;
             return -1.0;
         }
         default: {
@@ -194,11 +167,6 @@ void errorUpdate(vector<double> _layer, double* _error, double _t, double _h, in
     for (int i = 0; i < _layer.size(); ++i) {
         double exact = exactSolution(_t, i * _h, _testNum);
         double difference = fabs(_layer[i] - exact);
-        if (fabs(difference - 1.64) <= 1e-5) {
-            cout << "t = " << _t << endl;
-            cout << "x = " << i * _h << endl;
-        }
-        //cout << "difference = " << difference << endl;
         if (difference > *_error) {
             *_error = difference;
         }
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     string path = "../data/waveOut.dat";
-    double t = 0.1;
+    double t = 0.01;
     double h = 0.1;
     crossScheme(t, h, path, 2);
     return 0;
