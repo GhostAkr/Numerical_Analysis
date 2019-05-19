9b627519800c15ec449b0f91d5141d109930df4a
Alexander Antoshkin <ighost99@gmail.com>
2019-05-19 15:50:04 +0300

    Service functions were added.

--- project/include/PoissonEquation.h
+++ project/include/PoissonEquation.h
@@ -6,6 +6,7 @@
 #define NUMERICAL_ANALYSIS_POISSONEQUATION_H
 
 #include "../include/OrdinaryDE.h"
+#include "../include/HeatEquation.h"
 
 #include <iostream>
 #include <vector>
@@ -13,7 +14,21 @@
 using std::cout;
 using std::endl;
 using std::vector;
+using std::string;
 
+//Scheme
+void switchDirectionsScheme(string _path, double _t, double _h1, double _h2, int _testNum);
+
+// Task values
+double fFunction(double _x1, double _x2, int _testNum);
+
+//Service functions
+double lambda1(vector<vector<double>> _layer, double _h1, int _i, int _j);
+double lambda2(vector<vector<double>> _layer, double _h2, int _i, int _j);
+double F(vector<vector<double>> _layer, int _i, int _j, double _t, int _testNum, double _h1, double _h2);
+double F1(vector<vector<double>> _layer, int _i, int _j, double _t, int _testNum, double _h1, double _h2);
+
+// Other
 vector<vector<double>> mesh2D(double _L1, double _L2, double _h1, double _h2);
 
 #endif //NUMERICAL_ANALYSIS_POISSONEQUATION_H
--- project/src/PoissonEquation.cpp
+++ project/src/PoissonEquation.cpp
@@ -10,3 +10,39 @@ vector<vector<double>> mesh2D(double _L1, double _L2, double _h1, double _h2) {
     vector<vector<double>> result (N1, vector<double> (N2, 0));
     return result;
 }
+
+double lambda1(vector<vector<double>> _layer, double _h1, int _i, int _j) {
+    double result = (_layer[_i + 1][_j] - 2.0 * _layer[_i][_j] + _layer[_i - 1][_j]) / (_h1 * _h1);
+    return result;
+}
+
+double lambda2(vector<vector<double>> _layer, double _h2, int _i, int _j) {
+    double result = (_layer[_i][_j + 1] - 2.0 * _layer[_i][_j] + _layer[_i][_j - 1]) / (_h2 * _h2);
+    return result;
+}
+
+double fFunction(double _x1, double _x2, int _testNum) {
+    switch (_testNum) {
+        case 1: {
+            return 0;
+        }
+        default: {
+            cout << "Exception in fFunction" << endl;
+            return -1;
+        }
+    }
+}
+
+double F(vector<vector<double>> _layer, int _i, int _j, double _t, int _testNum, double _h1, double _h2) {
+    double y = _layer[_i][_j];
+    double phi = fFunction(_i * _h1, _j * _h2, _testNum);
+    double result = 2.0 / _t * y + lambda2(_layer, _h2, _i, _j) + phi;
+    return result;
+}
+
+double F1(vector<vector<double>> _layer, int _i, int _j, double _t, int _testNum, double _h1, double _h2) {
+    double y = _layer[_i][_j];
+    double phi = fFunction(_i * _h1, _j * _h2, _testNum);
+    double result = 2.0 / _t * y + lambda1(_layer, _h1, _i, _j) + phi;
+    return result;
+}
