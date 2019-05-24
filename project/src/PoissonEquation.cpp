e004be4f17a3ff2c3de5eb744e678b25dd3a28a0
Alexander <ighost99@gmail.com>
2019-05-24 08:14:40 +0300

    Temporary commit.

--- project/src/PoissonEquation.cpp
+++ project/src/PoissonEquation.cpp
@@ -5,9 +5,9 @@
 #include "../include/PoissonEquation.h"
 
 vector<vector<double>> mesh2D(double _L1, double _L2, double _h1, double _h2) {
-    int N1 = _L1 / _h1 + 2;
-    int N2 = _L2 / _h2 + 2;
-    vector<vector<double>> result (N1, vector<double> (N2, 0));
+    int N1 = _L1 / _h1 + 1;
+    int N2 = _L2 / _h2 + 1;
+    vector<vector<double>> result (N1 + 1, vector<double> (N2 + 1, 0));
     return result;
 }
 
@@ -54,7 +54,19 @@ vector<vector<double>> initLayer(double _L1, double _L2, double _h1, double _h2,
         case 1: {
             for (int i = 0; i < result.size(); ++i) {
                 for (int j = 0; j < result[0].size(); ++j) {
-                    result[i][j] = 1.0;
+                    result[i][j] = 1000.0;
+                    if (i == 0) {
+                        result[i][j] = 1.0;
+                    }
+                    if (i == result.size() - 1) {
+                        result[i][j] = 1.0;
+                    }
+                    if (j == 0) {
+                        result[i][j] = 1.0;
+                    }
+                    if (j == result.size() - 1) {
+                        result[i][j] = 1.0;
+                    }
                 }
             }
             return result;
@@ -70,6 +82,8 @@ double F(vector<vector<double>> _layer, int _i, int _j, double _t, int _testNum,
     double y = _layer[_i][_j];
     double phi = fFunction(_i * _h1, _j * _h2, _testNum);
     double result = 2.0 / _t * y + lambda2(_layer, _h2, _i, _j) + phi;
+//    cout << "[i][j] = " << _layer[_i][_j] << endl;
+//    cout << "l2 = " << lambda2(_layer, _h2, _i, _j) << endl;
     return result;
 }
 
@@ -118,17 +132,22 @@ void switchDirectionsScheme(string _path, double _t, double _h1, double _h2, int
     }
     // Constants
     vector<double> L = area(_testNum);
-    int N1 = (L[0] / _h1) + 2;
-    int N2 = (L[1] / _h2) + 2;
+    int N1 = round((L[0] / _h1) + 1);
+    int N2 = round((L[1] / _h2) + 1);
+//    cout << "N1 = " << N1 << endl;
+//    cout << "N2 = " << N2 << endl;
+//    int N1 = 11;
+//    int N2 = 11;
+//    cout << "N2 = " << N2 << endl;
     double hh1 = _h1 * _h1;
     double hh2 = _h2 * _h2;
     double A1 = 1.0 / hh1;
-    double B1 = 2.0 * (1.0 / hh1 + 1.0 / _t);
+    double B1 = -2.0 * (1.0 / hh1 + 1.0 / _t);
     double C1 = 1.0 / hh1;
     double A2 = 1.0 / hh2;
-    double B2 = 2.0 * (1.0 / hh2 + 1.0 / _t);
+    double B2 = -2.0 * (1.0 / hh2 + 1.0 / _t);
     double C2 = 1.0 / hh2;
-    double T = 1.0;
+    double T = 2;
     // Null time layer
     vector<vector<double>> buffLayer = initLayer(L[0], L[1], _h1, _h2, _testNum);
     for (int i = 0; i < buffLayer.size(); ++i) {
@@ -140,9 +159,15 @@ void switchDirectionsScheme(string _path, double _t, double _h1, double _h2, int
     // Main algorithm
     double currentTime = 0;
     while (currentTime <= T) {
+        cout << "buffLayer before" << endl;
+        matrixPrint(buffLayer);
         vector<vector<double>> buffHalfLayer = mesh2D(L[0], L[1], _h1, _h2);
+//        cout << "buffHalfLayer" << endl;
+//        matrixPrint(buffHalfLayer);
         // First system
-        for (int j = 1; j < N1 - 1; ++j) {
+//        cout << "N2 = " << N2 << endl;
+        for (int j = 1; j < N2 - 1; ++j) {
+            vector<double> borders = border1(j * _h2, _testNum);
             vector<vector<double>> triMatrix1 (N1 - 2, vector<double> (4, 0));
             triMatrix1[0][1] = B1;
             triMatrix1[0][2] = C1;
@@ -153,40 +178,73 @@ void switchDirectionsScheme(string _path, double _t, double _h1, double _h2, int
             }
             triMatrix1[triMatrix1.size() - 1][0] = A1;
             triMatrix1[triMatrix1.size() - 1][1] = B1;
+//            cout << "Size 1 = " << triMatrix1.size() << endl;
+//            cout << "Size 1 = " << buffLayer.size() << endl;
+//            cout << "Size 2 = " << buffLayer[0].size() << endl;
             for(int i = 0; i < triMatrix1.size(); ++i) {
-                triMatrix1[i][3] = -F(buffLayer, i, j, _t, _testNum, _h1, _h2);
+//                cout << "j = " << j << endl;
+//                cout << "i = " << i << endl;
+                triMatrix1[i][3] = -F(buffLayer, i + 1, j, _t, _testNum, _h1, _h2);
+                if (i == 0) {
+                    triMatrix1[i][3] -= A1 * borders[0];
+                }
+                if (i == triMatrix1.size() - 1) {
+                    triMatrix1[i][3] -= C1 * borders[1];
+                }
             }
+//            cout << "triMatrix1" << endl;
+//            matrixPrint(triMatrix1);
+//            cout << "Test" << endl;
             vector<double> halfSolution = tridiagonalLinearSolve(triMatrix1);
-            vector<double> borders = border1(j * _h2, _testNum);
+//            cout << "SSSSuka = " << halfSolution.size() << endl;
             halfSolution.insert(halfSolution.begin(), borders[0]);
             halfSolution.push_back(borders[1]);
-            buffHalfLayer[j] = halfSolution;
+//            cout << "halfSolution" << endl;
+//            vectorPrint(halfSolution);
+            for (int k = 0; k < buffHalfLayer.size(); ++k) {
+                buffHalfLayer[k][j] = halfSolution[k];
+            }
         }
-        for(int j = 0; j < buffHalfLayer[0].size(); ++j) {
-            vector<double> borders = border2(j * _h1, _testNum);
-            buffHalfLayer[j][0] = borders[0];
-            buffHalfLayer[j][buffHalfLayer.size() - 1] = borders[1];
+//        cout << "buffHalfLayer" << endl;
+//        matrixPrint(buffHalfLayer);
+
+        for(int i = 0; i < buffHalfLayer.size(); ++i) {
+            vector<double> borders = border2(i * _h1, _testNum);
+            buffHalfLayer[i][0] = borders[0];
+            buffHalfLayer[i][buffHalfLayer.size() - 1] = borders[1];
         }
         // Second system
-        for (int j = 1; j <= N2 - 1; ++j) {
-            vector<vector<double>> triMatrix2 (N2 - 1, vector<double> (4, 0));
+//        cout << "N1 = " << N1 << endl;
+        for (int i = 1; i < N1 - 1; ++i) {
+            vector<double> borders = border2(i * _h1, _testNum);
+            vector<vector<double>> triMatrix2 (N2 - 2, vector<double> (4, 0));
             triMatrix2[0][1] = B2;
             triMatrix2[0][2] = C2;
-            for(int i = 1; i < N2 - 2; ++i) {
-                triMatrix2[i][0] = A2;
-                triMatrix2[i][1] = B2;
-                triMatrix2[i][2] = C2;
+            for(int j = 1; j <= triMatrix2.size() - 2; ++j) {
+                triMatrix2[j][0] = A2;
+                triMatrix2[j][1] = B2;
+                triMatrix2[j][2] = C2;
             }
-            triMatrix2[N2 - 2][0] = A2;
-            triMatrix2[N2 - 2][1] = B2;
-            for(int i = 0; i < N2 - 1; ++i) {
-                triMatrix2[i][3] = -F1(buffHalfLayer, j, i + 1, _t, _testNum, _h1, _h2);
+            triMatrix2[triMatrix2.size() - 1][0] = A2;
+            triMatrix2[triMatrix2.size() - 1][1] = B2;
+//            cout << "Size 2 = " << triMatrix2.size() << endl;
+//            cout << "SSSize 1 = " << buffHalfLayer.size() << endl;
+//            cout << "SSSize 2 = " << buffHalfLayer[0].size() << endl;
+            for(int j = 0; j < triMatrix2.size(); ++j) {
+//                cout << "j = " << j << endl;
+//                cout << "i = " << i << endl;
+                triMatrix2[j][3] = -F1(buffHalfLayer, i, j + 1, _t, _testNum, _h1, _h2);
+                if (j == 0) {
+                    triMatrix2[j][3] -= A2 * borders[0];
+                }
+                if (j == triMatrix2.size() - 1) {
+                    triMatrix2[j][3] -= C2 * borders[1];
+                }
             }
             vector<double> halfSolution = tridiagonalLinearSolve(triMatrix2);
-            vector<double> borders = border2(j * _h1, _testNum);
             halfSolution.insert(halfSolution.begin(), borders[0]);
             halfSolution.push_back(borders[1]);
-            buffHalfLayer[j] = halfSolution;
+            buffHalfLayer[i] = halfSolution;
         }
         for(int j = 0; j < buffHalfLayer[0].size(); ++j) {
             vector<double> borders = border1(j * _h2, _testNum);
@@ -194,11 +252,18 @@ void switchDirectionsScheme(string _path, double _t, double _h1, double _h2, int
             buffHalfLayer[j][buffHalfLayer.size() - 1] = borders[1];
         }
         buffLayer = buffHalfLayer;
+//        cout << "buffLayer after" << endl;
+//        matrixPrint(buffLayer);
+//        cout << "buffLayer" << endl;
+//        matrixPrint(buffLayer);
+//        cout << "Size = " << buffLayer[1].size() << endl;
+//        cout << "qqepta = " << buffLayer[1][buffLayer[0].size() - 1] << endl;
         for (int i = 0; i < buffLayer.size(); ++i) {
             for (int j = 0; j < buffLayer[0].size(); ++j) {
                 fOut << buffLayer[i][j] << " ";
             }
         }
+
         fOut << endl;
         currentTime += _t;
     }
